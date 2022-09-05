4642fb0c9b0fc8129ab86c26aa969b826e2e1a7d
Jordi Duran <jordiduran99@gmail.com>
2022-09-05 13:33:28 +0200

    Create project

---
+++ TestingExample/TestingExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestingExample
+//
+//  Created by Jordi Duran Ortega on 5/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
